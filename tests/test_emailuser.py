d0c146884b5a0b5c6a31ff674699969fbf49ebcd
simlist <simlist@gmail.com>
2019-07-31 08:59:32 -0500

    Moved tests to top level directory.

--- tests/test_emailuser.py
+++ tests/test_emailuser.py
@@ -3,7 +3,7 @@ from django.urls import reverse
 from rest_framework import status
 from rest_framework.test import APIClient, APITestCase
 
-from .models import EmailUser
+from emailuser.models import EmailUser
 
 EMAIL = 'me@example.com'
 NAME = 'Foo Bar'
