b059cae4c715f9f4ed0e39c45d5585ddf4073c6d
simlist <simlist@gmail.com>
2019-08-04 23:06:29 -0500

    Changed url for register.

--- emailuser/urls.py
+++ emailuser/urls.py
@@ -6,9 +6,8 @@ from . import views
 
 app_name = 'accounts'
 urlpatterns = [
-    path('register/', views.RegistrationView.as_view(), name='register'),
+    path('users/register/', views.RegistrationView.as_view(), name='register'),
     path('users/<int:pk>', views.RetrieveUpdateUserView.as_view(), name='update'),
-
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
