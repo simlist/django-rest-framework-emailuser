0ff51c03d5a750b6d00901c94ed97dc18b84db51
MS List <simlist@gmail.com>
2023-07-11 11:01:29 -0400

    Removed Meta parent class.

--- src/emailuser/forms.py
+++ src/emailuser/forms.py
@@ -3,12 +3,12 @@ from django.contrib.auth.forms import UserCreationForm, UserChangeForm
 
 
 class EmailUserCreationForm(UserCreationForm):
-    class Meta(UserCreationForm):
+    class Meta():
         model = get_user_model()
         fields = ('email', 'name')
 
 
 class EmailUserChangeForm(UserChangeForm):
-    class Meta:
+    class Meta():
         model = get_user_model()
         fields = ('email', 'name')
